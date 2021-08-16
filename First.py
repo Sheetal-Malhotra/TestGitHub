2d88b3d19220e72f319efb911bdc8e7c046be1d9
Sheetal-Malhotra <75935060+Sheetal-Malhotra@users.noreply.github.com>
2021-08-16 20:29:11 +0530

    Added new files

---
+++ First.py
@@ -0,0 +1,6 @@
+Hungry = input("are you hungry")
+if Hungry == "Yes"
+	print("Eat something")
+else
+	print("do your work")
+
---
+++ Second.py
@@ -0,0 +1,8 @@
+Thirsty= input("are you thirsty")
+if thirsty == "Yes"
+	print("Drink Coke")
+else if
+	print("Drink Mojito")
+else
+	print("do your work")
+
